1b673f2e12648ab0d5e362e8197ca504de06b535
muhammad-naveed <m.naveed.ch@gmail.com>
2017-08-17 10:22:26 +0500

    added placeholder home page for now.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = require('./config');
 
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
